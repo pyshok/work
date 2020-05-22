c8129c9770d9f297d22304d00df0cb718f67abb4
pyshok <barinov_tosha@inbox.ru>
2020-05-22 12:31:02 -0400

    finding factorial

--- algoritms/Program.cs
+++ algoritms/Program.cs
@@ -4,8 +4,28 @@ namespace algoritms
 {
     class Program
     {
+        //finding factorial by recursive method
+        static int Factorial(int x){
+            if (x==0){
+                return 1;
+            }
+            else {
+                return x * Factorial(x-1);
+            }
+        }
+         //finding factorial by an interactive method
+        static int Fac(int x){
+            int res = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                 res = i*res;
+            }
+            return res;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Fac(6));
+            Console.WriteLine(Factorial(6));
             Console.WriteLine("Hello World!");
         }
     }
